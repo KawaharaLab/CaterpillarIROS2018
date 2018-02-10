d47f20595e85aed459e909c6efdedd1c1054415a
Matthew Ishige <matthewlujp@gmail.com>
2018-02-10 12:15:08 +0900

    Add learning curve graph for separately learning discrepancy coefficient

--- util/renderer.py
+++ util/renderer.py
@@ -1,5 +1,6 @@
 import bpy
-from pickle import load
+import pickle
+import json
 import math
 import sys
 
@@ -18,9 +19,20 @@ class Renderer:
         self._objects = {}
         self._on_animation = False
 
-    def load_simulation(self, file_path):
-        with open(file_path, 'rb') as f:
-            simulation_protocol = load(f)
+    def load_simulation(self, file_path: str, file_type: str):
+        """
+        run simulation saved in file_path
+
+        file_path: where simulation result is saved
+        file_type: simulation type. pickle and json is supported
+        """
+        if file_type == 'json':
+            simulation_protocol = self._load_json(file_path)
+        elif file_type == 'pickle':
+            simulation_protocol = self._load_pickle(file_path)
+        else:
+            raise Exception("invalid file type")
+
         # Draw objects
         objects = simulation_protocol['objects']
         for (r, pos, name) in objects:
@@ -35,6 +47,18 @@ class Renderer:
             for (n, pos) in motions:
                 self.move_object(n, pos)
         self.end_simulation()
+
+    def _load_json(self, file_path:str) -> dict:
+        with open(file_path, 'r') as f:
+            raw_sim_proc = json.load(f)
+        sim_proc = {}
+        sim_proc['objects'] = [(o['rad'], tuple(o['pos']), o['id']) for o in raw_sim_proc['objects']]
+        sim_proc['frames'] = {int(i): [(o['id'], tuple(o['pos'])) for o in v] for i, v in  raw_sim_proc['frames'].items()}
+        return sim_proc
+
+    def _load_pickle(self, file_path:str) -> dict:
+        with open(file_path, 'rb') as f:
+            return pickle.load(f)
 
     def delete_objects(self):
         for item in self._scene.objects:
@@ -113,7 +137,9 @@ class Renderer:
 
 
 renderer = Renderer()
-file_path = sys.argv[-1]
-renderer.load_simulation(file_path)
+file_path = sys.argv[1]
+file_type = sys.argv[2] if len(sys.argv) >= 3 else 'json'
+print("file type {}".format(file_type))
+renderer.load_simulation(file_path, file_type)
 # renderer.start_simulation()
 # renderer.end_simulation()
